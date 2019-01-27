a4248d7095caaa51433d12755e15a0344910c863
Christina Landvytovych <Crofty.land@gmail.com>
2019-01-27 23:37:29 +0200

    head animation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // });
 //
 // tl.to("#bb8-body",1, {rotation:"+=720"});
-var bb8Body = document.getElementById("bb8-body");
+let bb8Body = document.getElementById("bb8-body");
 TweenMax.set(bb8Body, {
     transformOrigin:"50% 50%"
 });
@@ -21,5 +21,13 @@ TweenMax.to(bb8Body,1, {
     repeat: -1,
     paused: false});
 
-var bb8Shadow = document.getElementById("bb8-shadow");
+let bb8Shadow = document.getElementById("bb8-shadow");
 TweenMax.to(bb8Shadow, 1,{scale:1.2, repeat:-1, yoyo:true});
+
+    // mainTl = new TimelineMax({repeat: -1, repeatDelay: 1});
+
+let bb8Head = document.getElementById("bb8-head");
+
+
+TweenMax.to(bb8Head, 3, {rotation:40, x:40, y:130, transformOrigin:"530px 890px", repeat:10, easeInOut:Bounce, yoyo: true});
+TweenLite.to(bb8Head, 4, { ease: Back.easeOut.config(1.7), y: -10 });
